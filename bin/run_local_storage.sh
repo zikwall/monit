b0599854c12c3d718a6c384ec9182e1f89ebefd1
zikwall <andrey.kapitonov.96@gmail.com>
2021-11-07 19:16:30 +0300

    Remove script

--- bin/run_local_storage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-go run ./src/services/storage/  \
-  --bind-address localhost:7000 \
-  --clickhouse-address 194.35.48.20 \
-  --clickhouse-user default \
-  --clickhouse-database default \
-  --debug
